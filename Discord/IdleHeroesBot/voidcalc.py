e8322ef4ecc4c2eb32d121a31c651edc152b48cb
Xiaika <103392935+Xiaika@users.noreply.github.com>
2022-06-21 00:45:39 -0500

    Add files via upload

--- Discord/IdleHeroesBot/voidcalc.py
+++ Discord/IdleHeroesBot/voidcalc.py
@@ -1,25 +1,20 @@
 from data import realms, tiers, sectors
 
-# Constants - Variables to implement inputs later
-PRIVILEGE = True
-BS_CLEAR = True
-VIP_LEVEL = 10
+# Actual Constants
 MAX_ENERGY = 240
-CORES_MARKET = 30
-CORES_BOUGHT = 0
-CORES_HALL = 10
-
 VORTEX_CYCLE_PER_WEEK = 2 / 7
-MULTIPLIER = 1
-if PRIVILEGE:
-    MULTIPLIER += 0.15
-if BS_CLEAR:
-    MULTIPLIER += 0.15
 
+# Constants - Variables to implement inputs from roles
+
+
+# Extra command line input
+CORES_MARKET = 0
+CORES_BOUGHT = 0
+CORES_HALL = 0
 
 # ------------------------------------------- Cores -----------------------------------------------------------
-def core_day(spheres):
-    monthly_cores = CORES_MARKET + CORES_BOUGHT + CORES_HALL
+def core_day(spheres, market=30, bought=0, hall=0):
+    monthly_cores = market + bought + hall
     cot = round(monthly_cores * 8000 / 30)
     stellar = 0
     match spheres:
@@ -33,32 +28,10 @@ def core_day(spheres):
     return cot, stellar
 
 
-core_day('Stellar')
-
-
 # ------------------------------------------ VIP Chest ---------------------------------------------------------
-def vip_chests_day(vip, choice):
+def vip_chests_day(choice, chests):
     cot = 0
     stellar = 0
-    if isinstance(vip, int):
-        if vip < 0:
-            raise ValueError('VIP must be positive')
-        elif vip == 0:
-            chests = 0
-        elif vip < 5:
-            chests = 1
-        elif vip < 7:
-            chests = 2
-        elif vip < 9:
-            chests = 3
-        elif vip < 11:
-            chests = 4
-        elif vip <= 13:
-            chests = 5
-        else:
-            raise ValueError('VIP must be a positive integer no greater than 13')
-    else:
-        raise ValueError('VIP must be of type integer')
     if choice == 'None':
         return cot, stellar
     elif choice == 'CoT':
@@ -66,20 +39,18 @@ def vip_chests_day(vip, choice):
     elif choice == 'Stellar':
         stellar = 2500 * chests
     else:
-        raise ValueError('Invalid choice for chest contents')
+        raise ValueError('Invalid Role for chest contents')
     return cot, stellar
 
 
 # ----------------------------------------- Void Vortex --------------------------------------------------------
-def vortex_day(tier):
+def vortex_day(tier, MULTIPLIER):
     cot, stellar = tiers[tier].values()
     cot = round(cot * MULTIPLIER * VORTEX_CYCLE_PER_WEEK)
     stellar = round(stellar * MULTIPLIER * VORTEX_CYCLE_PER_WEEK)
     # print(cot, stellar)
     return cot, stellar
 
-
-vortex_day('Vanquisher I')
 
 # ----------------------------------------- Realms Gate --------------------------------------------------------
 # Route Data
@@ -88,11 +59,9 @@ chest_route = {'Total': {'Phantoms': 189.95, 'Puppets': 10.02, 'Lords': 1, 'Ches
 
 no_chest_route = {'Total': {'Phantoms': 192.31, 'Puppets': 9.89, 'Lords': 1, 'Chests': 8.53, 'Mines': 4},
                   'Energy': {'Phantoms': 961.57, 'Puppets': 98.89, 'Lords': 10, 'Chests': 85.33, 'Mines': 40}}
-# corruption_level = str(input('Enter corruption level:  '))
-corruption_level = '108'
 
 
-def realm_cycle(level, route):
+def realm_cycle(level, route, MULTIPLIER):
     # TODO: Check accuracy later but very good for now
     cot_values = [round(MULTIPLIER * value) for value in realms[level]['CoT'].values()]
     stellar_values = [round(MULTIPLIER * value) for value in realms[level]['Stellar'].values()]
@@ -111,15 +80,13 @@ def realm_cycle(level, route):
     return cot_cycle, stellar_cycle
 
 
-def realm_day(level, route):
+def realm_day(level, route, MULTIPLIER):
     energy_total = sum(route['Energy'].values())
-    cycle = realm_cycle(level, route)
+    cycle = realm_cycle(level, route, MULTIPLIER)
     cot_per_day = [round(value / energy_total * MAX_ENERGY) for value in cycle[0]]
     stellar_per_day = [round(value / energy_total * MAX_ENERGY) for value in cycle[1]]
     return sum(cot_per_day), sum(stellar_per_day)
 
-
-# print(realm_day(corruption_level, chest_route))
 
 # TODO: Void Ark Calculations (Fine for now make inputs variable)
 HERO_COUNT = 15
@@ -153,7 +120,7 @@ for index, rank in enumerate(HEROES):
 total_multiplier = sum(hero_multipliers)
 
 
-def ark_day():
+def ark_day(MULTIPLIER):
     lodes3 = round(sum(Nmaxelements(sorted(hero_multipliers, reverse=True), N=3)), 3)
     non3 = (total_multiplier - lodes3) / 12
     lodes4 = round(sum(Nmaxelements(sorted(hero_multipliers, reverse=True), N=4)), 3)
@@ -175,8 +142,49 @@ def ark_store_day(cot, stellar):
     return round(cot / 30), round(stellar / 30)
 
 
-def total_void(corruption, vortex_tier, core_choice, hall_cot, hall_stellar, vip_level, chest_choice):
-    total_cot = realm_day(corruption, chest_route)[0] + vortex_day(vortex_tier)[0] + core_day(core_choice)[0] + ark_store_day(hall_cot, hall_stellar)[0] + ark_day()[0] + vip_chests_day(vip_level, chest_choice)[0]
-    total_stellar = realm_day(corruption, chest_route)[1] + vortex_day(vortex_tier)[1] + core_day(core_choice)[1] + ark_store_day(hall_cot, hall_stellar)[1] + ark_day()[1] + vip_chests_day(vip_level, chest_choice)[1]
+def total_void(vortex_tier, corruption, core_choice, chests, chest_choice='None', hall_cot=0, hall_stellar=0,
+               bs=False, privilege=False, market_cores=30, bought_cores=0, hall_cores=0, extra=False):
+    # TODO: add support for dominator
+    MULTIPLIER = 1
+    if bool(privilege):
+        MULTIPLIER += 0.15
+    if bool(bs):
+        MULTIPLIER += 0.15
+
+    vortex_cot, vortex_stellar = vortex_day(vortex_tier, MULTIPLIER)
+    realm_cot, realm_stellar = realm_day(corruption, chest_route, MULTIPLIER)
+    core_cot, core_stellar = core_day(core_choice, market_cores, bought_cores, hall_cores)
+    ark_store_cot, ark_store_stellar = ark_store_day(hall_cot, hall_stellar)
+    ark_cot, ark_stellar = ark_day(MULTIPLIER)
+    vip_cot, vip_stellar = vip_chests_day(chest_choice, chests)
+
+    print(f'Vortex {vortex_cot, vortex_stellar}')
+    print(f'Realms {realm_cot, realm_stellar}')
+    print(f'Cores  {core_cot, core_stellar}')
+    print(f'Ark Store {ark_store_cot, ark_store_stellar}')
+    print(f'Ark {ark_cot, ark_stellar}')
+    print(f'VIP {vip_cot, vip_stellar}')
+    print('--------------------------------------')
+
+    total_cot = vortex_cot + realm_cot + core_cot + ark_store_cot + ark_cot + vip_cot
+    total_stellar = vortex_stellar + realm_stellar + core_stellar + ark_store_stellar + ark_stellar + vip_stellar
+
+    if extra:
+        cot_list = list()
+        stellar_list = list()
+        cot_list.append(vortex_cot)
+        cot_list.append(realm_cot)
+        cot_list.append(core_cot)
+        cot_list.append(ark_store_cot)
+        cot_list.append(ark_cot)
+        cot_list.append(vip_cot)
+        stellar_list.append(vortex_stellar)
+        stellar_list.append(realm_stellar)
+        stellar_list.append(core_stellar)
+        stellar_list.append(ark_store_stellar)
+        stellar_list.append(ark_stellar)
+        stellar_list.append(vip_stellar)
+        return cot_list, stellar_list, total_cot, total_stellar
+
     return total_cot, total_stellar
 
